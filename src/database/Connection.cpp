72d2b757167fd07b7a7693b98ffdaab98c132281
Michal Slusarczyk <ms@mail.com>
2022-08-05 17:57:00 +0200

    Obiekt sqlit3 jako shared_ptr

--- src/database/Connection.cpp
+++ src/database/Connection.cpp
@@ -13,39 +13,39 @@ class Connection::Impl {
 public:
   Impl(const std::string_view &connectionString);
   Impl();
-  ~Impl();
 
   sqlite3 *getRawConnection();
 
 private:
-  sqlite3 *m_dbConnection;
+  std::shared_ptr<sqlite3> m_dbConnection;
 };
 
 Connection::Impl::Impl(const std::string_view &connectionString) {
   const auto flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
+  sqlite3 *connection;
   const auto result =
-      sqlite3_open_v2(connectionString.data(), &m_dbConnection, flags, nullptr);
+      sqlite3_open_v2(connectionString.data(), &connection, flags, nullptr);
   if (result != SQLITE_OK) {
     throw std::runtime_error(
         fmt::format("Cannot open database '{}': error code {:x}",
                     connectionString, result));
   }
+  m_dbConnection = std::shared_ptr<sqlite3>{connection, &sqlite3_close};
 }
 
 Connection::Impl::Impl() {
   const auto flags =
       SQLITE_OPEN_MEMORY | SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
-  const auto result =
-      sqlite3_open_v2(":memory:", &m_dbConnection, flags, nullptr);
+  sqlite3 *connection;
+  const auto result = sqlite3_open_v2(":memory:", &connection, flags, nullptr);
   if (result != SQLITE_OK) {
     throw std::runtime_error(fmt::format(
         "Cannot open database '{}': error code {:x}", ":memory:", result));
   }
+  m_dbConnection = std::shared_ptr<sqlite3>{connection, &sqlite3_close};
 }
 
-Connection::Impl::~Impl() { sqlite3_close(m_dbConnection); }
-
-sqlite3 *Connection::Impl::getRawConnection() { return m_dbConnection; }
+sqlite3 *Connection::Impl::getRawConnection() { return m_dbConnection.get(); }
 
 Connection::Connection() : m_impl(std::make_shared<Connection::Impl>("")) {}
 
