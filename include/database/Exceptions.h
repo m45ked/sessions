e8faa0250830c6f97892b8a5bcccd67bc2451034
Michal Slusarczyk <ms@mail.com>
2022-08-06 19:31:43 +0200

    Poprawienie ctora IncorrectQuerySql

--- include/database/Exceptions.h
+++ include/database/Exceptions.h
@@ -3,14 +3,12 @@
 #include <stdexcept>
 #include <string_view>
 
-#include "fmt/format-inl.h"
-
 namespace Database {
 
 class IncorrectQuerySql : public std::runtime_error {
 public:
   IncorrectQuerySql(const std::string_view &msg)
-      : std::runtime_error(fmt::format("Incorrect SQL: '%s'", msg)) {}
+      : std::runtime_error(msg.data()) {}
 };
 
 } // namespace Database
