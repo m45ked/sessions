7b0ec583d4b278bf4ccf95cc3d4ef3b92f592b1c
Mas Ked Rin <mas.ked.rin+git@gmail.com>
2023-08-15 16:31:33 +0200

    core.type_traits: usunięcie zbędnego typedefa

--- src/core/type_traits/is_optional_v.h
+++ src/core/type_traits/is_optional_v.h
@@ -7,9 +7,7 @@ namespace Core::type_traits {
 template <typename ValueT> struct is_optional : std::false_type {};
 
 template <typename ValueT>
-struct is_optional<std::optional<ValueT>> : std::true_type {
-  typedef ValueT value_type;
-};
+struct is_optional<std::optional<ValueT>> : std::true_type {};
 
 template <class T> inline constexpr bool is_optional_v = is_optional<T>::value;
 
