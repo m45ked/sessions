714be0e4a9e5bacb850e3e4c4174b3c5344661e6
Mas Ked Rin <mas.ked.rin+git@gmail.com>
2023-08-15 15:05:22 +0200

    database.Query: użycie is_optional_v

--- src/database/Query.h
+++ src/database/Query.h
@@ -11,6 +11,7 @@
 #include <type_traits>
 #include <vector>
 
+#include "core/type_traits/is_optional_v.h"
 #include "database/Connection_fwd.h"
 #include "database/Query_fwd.h"
 #include "database/database_export.h"
@@ -20,19 +21,11 @@ namespace Database {
 
 namespace detail {
 
-template <typename ValueT> struct is_optional : std::false_type {};
-
-template <typename ValueT>
-struct is_optional<std::optional<ValueT>> : std::true_type {
-  typedef ValueT value_type;
-};
-
 template <typename ValueT>
 auto getFromQuery(sqlite3_stmt *stmt, int idx) -> ValueT;
 
 template <typename ValueT>
-auto getOptionalFromQuery(sqlite3_stmt *stmt, int idx) -> ValueT
-{
+inline auto getOptionalFromQuery(sqlite3_stmt *stmt, int idx) -> ValueT {
   if (sqlite3_column_type(stmt, idx) == SQLITE_NULL)
     return std::nullopt;
 
@@ -50,8 +43,7 @@ public:
 
   template <typename ValueT>
   auto get(const std::string_view &fieldName) -> ValueT {
-    if constexpr (detail::is_optional<ValueT>::value)
-    {
+    if constexpr (Core::type_traits::is_optional_v<ValueT>) {
       return detail::getOptionalFromQuery<ValueT>(
           getRawStatement(), getColumnIdxFromStatement(fieldName));
     }
