2ee5a2b73e41fab02ebd388b672fe407500547c8
Mas Ked Rin <mas.ked.rin+git@gmail.com>
2023-08-15 16:33:20 +0200

    database.tests: poprawa testów

--- src/database/tests/queryTests.cpp
+++ src/database/tests/queryTests.cpp
@@ -13,44 +13,46 @@
 
 namespace {
 
+using Q = Database::Query;
+
 class QueryTest : public ::testing::Test {
 protected:
   Database::Connection m_conn;
 };
 
 TEST_F(QueryTest, createSimpleQuery) {
-  auto query = Database::Query{R"sql(select 1 'id')sql", m_conn};
+  auto query = Q{R"sql(select 1 'id')sql", m_conn};
   query.execute();
 }
 
 TEST_F(QueryTest, createIncorrectQuery) {
-  EXPECT_THROW(Database::Query(R"sql(select)sql", m_conn),
+  EXPECT_THROW(Q(R"sql(select)sql", m_conn),
                Database::IncorrectQuerySql);
 }
 
 TEST_F(QueryTest, getColumnValue_int) {
-  auto query = Database::Query(R"sql(select 1 id)sql", m_conn);
+  auto query = Q(R"sql(select 1 id)sql", m_conn);
   query.execute();
 
   EXPECT_EQ(query.get<int64_t>("id"), 1);
 }
 
 TEST_F(QueryTest, getColumnValue_double) {
-  auto query = Database::Query(R"sql(select 1.5 id)sql", m_conn);
+  auto query = Q(R"sql(select 1.5 id)sql", m_conn);
   query.execute();
 
   EXPECT_EQ(query.get<double>("id"), 1.5);
 }
 
 TEST_F(QueryTest, getColumnValue_string) {
-  auto query = Database::Query(R"sql(select "field value" id)sql", m_conn);
+  auto query = Q(R"sql(select "field value" id)sql", m_conn);
   query.execute();
 
   EXPECT_EQ(query.get<std::string>("id"), "field value");
 }
 
 TEST_F(QueryTest, getColumnValue_blob) {
-  auto query = Database::Query(R"sql(select randomblob(16) id)sql", m_conn);
+  auto query = Q(R"sql(select randomblob(16) id)sql", m_conn);
   query.execute();
   auto f = query.get<std::vector<std::byte>>("id");
   const auto defaultValue = std::vector<std::byte>{16ul};
@@ -59,21 +61,21 @@ TEST_F(QueryTest, getColumnValue_blob) {
 }
 
 TEST_F(QueryTest, getColumnValue_optInt) {
-  auto query = Database::Query{R"sql(select null 'value')sql", m_conn};
+  auto query = Q{R"sql(select null 'value')sql", m_conn};
   query.execute();
 
   EXPECT_THAT(query.get<std::optional<int64_t>>("value"), ::testing::Eq(std::nullopt));
 }
 
 TEST_F(QueryTest, getColumnValue_optString) {
-  auto query = Database::Query{R"sql(select null 'value')sql", m_conn};
+  auto query = Q{R"sql(select null 'value')sql", m_conn};
   query.execute();
 
   EXPECT_THAT(query.get<std::optional<std::string>>("value"), ::testing::Eq(std::nullopt));
 }
 
 TEST_F(QueryTest, getColumnValue_optDouble) {
-  auto query = Database::Query{R"sql(select null 'value')sql", m_conn};
+  auto query = Q{R"sql(select null 'value')sql", m_conn};
   query.execute();
 
   EXPECT_THAT(query.get<std::optional<double>>("value"), ::testing::Eq(std::nullopt));
@@ -81,7 +83,7 @@ TEST_F(QueryTest, getColumnValue_optDouble) {
 
 
 TEST_F(QueryTest, getColumnValue_optBlob) {
-  auto query = Database::Query{R"sql(select null 'value')sql", m_conn};
+  auto query = Q{R"sql(select null 'value')sql", m_conn};
   query.execute();
 
   EXPECT_THAT(query.get<std::optional<std::vector<std::byte>>>("value"), ::testing::Eq(std::nullopt));
