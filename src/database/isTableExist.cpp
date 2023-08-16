c74a821fbda17e95e969fab4a1aefa473d2bcaec
Mas Ked Rin <mas.ked.rin+git@gmail.com>
2023-08-16 18:17:25 +0200

    database.isTableExist: poprawka implementacji

--- src/database/isTableExist.cpp
+++ src/database/isTableExist.cpp
@@ -1,6 +1,7 @@
 #include "isTableExist.h"
 
 #include <cstdint>
+#include <string>
 
 #include "database/Query.h"
 #include "spdlog/fmt/bundled/core.h"
@@ -9,10 +10,9 @@ namespace Database {
 
 bool isTableExist(Connection &conn, std::string_view tableName) {
   auto q = Query(
-      fmt::format(
-          R"sql(select count(1) 'c' from sqlite_master where type='table' and name='{}')sql",
-          tableName),
+      R"sql(select count(1) 'c' from sqlite_master where type='table' and name=:tableName)sql",
       conn);
+  q.set<std::string>("tableName", tableName.data());
   q.execute();
 
   return q.get<std::int64_t>("c") > 0;
