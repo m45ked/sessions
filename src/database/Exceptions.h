d1641d923dbb54bf853e49be648832e4c40df21d
Mas Ked Rin <mas.ked.rin+git@gmail.com>
2023-09-09 15:07:03 +0200

    database: Dodanie DatabaseRuntimeErrora, z którego dziedziczą pozostałe errory

--- src/database/Exceptions.h
+++ src/database/Exceptions.h
@@ -5,27 +5,33 @@
 
 namespace Database {
 
-class IncorrectQuerySql : public std::runtime_error {
+class DatabaseRuntimeError : public std::runtime_error {
 public:
-  IncorrectQuerySql(std::string_view msg) : std::runtime_error(msg.data()) {}
+  DatabaseRuntimeError(std::string_view msg) : std::runtime_error(msg.data()) {}
 };
 
-class ErrorOpeningDatabase : public std::runtime_error {
+class IncorrectQuerySql : public DatabaseRuntimeError {
 public:
-  ErrorOpeningDatabase(std::string_view msg) : std::runtime_error(msg.data()) {}
+  IncorrectQuerySql(std::string_view msg) : DatabaseRuntimeError(msg.data()) {}
 };
 
-class NoSuchSqlParameter : public std::runtime_error {
+class ErrorOpeningDatabase : public DatabaseRuntimeError {
+public:
+  ErrorOpeningDatabase(std::string_view msg)
+      : DatabaseRuntimeError(msg.data()) {}
+};
+
+class NoSuchSqlParameter : public DatabaseRuntimeError {
 public:
   NoSuchSqlParameter(std::string_view parameterName)
-      : std::runtime_error(""), parameterName(parameterName) {}
+      : DatabaseRuntimeError(""), parameterName(parameterName) {}
 
   std::string parameterName;
 };
 
-struct QueryError : public std::runtime_error {
+struct QueryError : public DatabaseRuntimeError {
   QueryError(int errorCode, std::string_view msg)
-      : std::runtime_error(msg.data()), errorCode(errorCode) {}
+      : DatabaseRuntimeError(msg.data()), errorCode(errorCode) {}
 
   int errorCode;
 };
